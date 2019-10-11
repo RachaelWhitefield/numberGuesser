84099131d23f4a3303861cd48486019677ec8b15
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-10-11 14:55:11 -0700

    added losing game logic and UI colors

--- app.js
+++ app.js
@@ -36,18 +36,48 @@ guessBtn.addEventListener("click", function() {
 
     // Check if won
     if(guess === winningNum) {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = "green";
-        // Set Message
-        setMessage(`${winningNum} is correct! You Win!`, "green");
+        // Game over - won
+
+        gameOver(true, `${winningNum} is correct! You Win!`);
 
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if(guessesLeft === 0) {
+        // Game over - lost
+
+        gameOver(false, `Game over! You lost.  The correct number was ${winningNum}`, "red");
+
+        } else {
+            // Game continues - answer wrong
+
+            // Change border color
+            guessInput.style.borderColor = "red";
+
+            // Clear Input
+            guessInput.value = "";
+
+            // Tell User it's the wrong number
+            setMessage(`${guess} is not correct.  ${guessesLeft} guesses left.`, "red");
+        }
     }
-  
 });
+
+// Game over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = "green" : color = "red";
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set Message
+    setMessage(msg);
+}
 
 // Set message
 function setMessage(msg, color) {
